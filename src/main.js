5ff83bc8b60bfd1b86f119650fc900d952d9f452
LucasS-05 <lucas.m.maris@gmail.com>
2023-08-27 13:03:38 +0300

    .

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./css/index.scss";
+
+import { initLightbox } from "./js/index.js";
+initLightbox();
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import kirby from "vite-plugin-kirby";
+
+const cwd = process.cwd();
+
+const input = {
+  main: "src/main.js"
+};
+
+export default defineConfig(({ mode }) => ({
+  root: "src",
+  base: mode === "development" ? "/" : "/assets/dist/",
+
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(cwd, "src") }]
+  },
+
+  build: {
+    outDir: resolve(cwd, "public/assets/dist"),
+    emptyOutDir: true,
+    rollupOptions: { input }
+  },
+
+  plugins: [kirby()]
+}));
